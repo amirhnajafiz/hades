6fb5b28aa1323838bb79bfb669a14be9eb790214
amirhnajafiz <najafizadeh21@gmail.com>
2023-02-07 13:30:19 +0330

    add: russell src

--- 05.curl/russell/src/main.rs
+++ 05.curl/russell/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let fact = get_cat_fact().await;
+
+    println!("fact = {:#?}", fact);
+}
+
+async fn get_cat_fact() -> Result<String, Box<dyn std::error::Error>> {
+    let client = reqwest::Client::new();
+    let body = client.get("https://catfact.ninja/fact").send()
+        .await?
+        .text()
+        .await?;
+
+    Ok(body)
 }
