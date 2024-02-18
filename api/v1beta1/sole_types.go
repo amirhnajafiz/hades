8d6d7e7ec4e9fa7426236c7ed15e5a0a6414d650
amirhnajafiz <najafizadeh21@gmail.com>
2024-02-18 18:00:46 +0330

    update: apis

--- api/v1beta1/sole_types.go
+++ api/v1beta1/sole_types.go
@@ -1,19 +1,3 @@
-/*
-Copyright 2024.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package v1beta1
 
 import (
@@ -23,27 +7,22 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // SoleSpec defines the desired state of Sole
 type SoleSpec struct {
 	// Job is the job spec of a cronjob manifest
-	Job v1.JobSpec
-	// Cronjob name
-	Cronjob string
-	// Number of retry for this job
-	Retry int
-	// Retry bound
-	Bound int
-	// Last try timestamp
-	LastTry time.Time
+	Job v1.JobSpec `json:"job"`
+	// Cronjob name (the body name)
+	Cronjob string `json:"cronjob"`
+	// Retry bound (an upper bound for the number of retry)
+	Bound int `json:"bound"`
 }
 
 // SoleStatus defines the observed state of Sole
 type SoleStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Number of retry for this job
+	Retry int `json:"retry"`
+	// Last try timestamp
+	LastTry time.Time `json:"last_try"`
 }
 
 //+kubebuilder:object:root=true
