5841148ce32dec58c568257052c73101041921d9
amirhnajafiz <najafizadeh21@gmail.com>
2024-06-25 17:42:50 +0330

    update: sole spec and sole status fields

--- api/v1alpha1/sole_types.go
+++ api/v1alpha1/sole_types.go
@@ -1,41 +1,26 @@
-/*
-Copyright 2024.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package v1alpha1
 
 import (
+	v1 "k8s.io/api/batch/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+// NOTE: Run "make generate, make manifests" to regenerate code after modifying this file.
 
 // SoleSpec defines the desired state of Sole
 type SoleSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of Sole. Edit sole_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// Job is bounded to a sole. Therefore, until the sole exists, it creates the job that is bounded to.
+	Job *v1.Job `json:"job,omitempty"`
+	// Interval is the time in seconds for a new retry. It will be used as reconcile interval.
+	Interval *int `json:"interval,omitempty"`
 }
 
 // SoleStatus defines the observed state of Sole
 type SoleStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Heal is a flag that determines if a job is being created for the sole or not. If true, it means
+	// that the sole has a job.
+	Heal bool `json:"heal,omitempty"`
 }
 
 //+kubebuilder:object:root=true
