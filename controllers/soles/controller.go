40e8f4ca506be55dd718291f2d9324b6bdb609e1
amirhnajafiz <najafizadeh21@gmail.com>
2024-06-25 18:09:23 +0330

    add: sole not found section

--- controllers/soles/controller.go
+++ controllers/soles/controller.go
@@ -2,9 +2,11 @@ package soles
 
 import (
 	"context"
+	"fmt"
 
 	hadesamirhnajafizv1alpha1 "github.com/amirhnajafiz/hades/api/v1alpha1"
 
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 )
@@ -18,8 +20,14 @@ import (
 func (r *Reconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	r.logger = log.FromContext(ctx)
 	r.sole = &hadesamirhnajafizv1alpha1.Sole{}
+	r.namespace = req.Namespace
 
-	// TODO(user): your logic here
+	// get sole object
+	switch err := r.Get(ctx, req.NamespacedName, r.sole); {
+	case apierrors.IsNotFound(err):
+		r.logger.Info(fmt.Sprintf("Sole %s in namespace %s not found!", req.Name, req.Namespace))
+		return ctrl.Result{}, nil
+	}
 
 	return ctrl.Result{}, nil
 }
